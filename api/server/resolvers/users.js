8458483c2ca65aea81f3d22383cc1c2ad8f8c5b8
lamgamelia <graceamelialam@gmail.com>
2023-03-26 18:33:14 +0800

    userError

---
+++ api/server/resolvers/users.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const User = require('../model/User');
+
+module.exports = {
+    Mutation: {
+        async register(_, {
+            registerInput: { username, email, password, confirmPassword }
+        }) {
+            encryptedPassword = await bcrypt.hash(password, 12);
+            const newUser = new User({
+                email,
+                username,
+                password: encryptedPassword.toLowerCase(),
+                createdAt: new Date().toISOString()
+            });
+
+            const res = await newUser.save();
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, "SECRET_KEY", { expiresIn: '1h' });
+            
+            return {
+                ...res._doc,
+                id: res_id,
+                token
+            };
+        }
+    }
+};
