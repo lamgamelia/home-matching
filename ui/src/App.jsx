d874c736d26562ef82a213388158b77da2a26550
lamgamelia <graceamelialam@gmail.com>
2023-04-26 00:34:48 +0800

    login-dropdown

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -20,92 +20,109 @@ import {Register} from "./pages/Register.jsx";
 
 function LoginStatus(props) {
   const { user, logout } = useContext(AuthContext);
-  const loginText = user ? (
-    <p>{user.username} 
-    <Link to="/home" onClick={logout} className="nav-link" style={{ color: "rgb(0,0,0)" }}>
-      Logout
-    </Link>
-    </p>
+  
+  return user ? (
+    <li className="h6 nav-item dropdown show">
+      <a className="nav-link dropdown-toggle d-flex align-items-center" href="#" role="button" id="navbardropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ color: "rgb(0,0,0)" }}>
+        <i className="bi bi-person-circle" style={{ color: "rgb(0,0,0)" }}></i>
+      </a>
+      <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+        <p className="dropdown-item disabled" style={{ color: "rgb(0,0,0)" }}>
+          <i className="bi bi-person-circle" style={{ color: "rgb(0,0,0)" }}></i>
+          <span> {user.username}</span>
+        </p>
+        <Link to="/" className="dropdown-item" href="#" onClick={logout}>Logout</Link>
+      </ul>
+    </li>
   ) : (
-    <Link to="/login" className="nav-link" style={{ color: "rgb(0,0,0)" }}>
-      Login/Sign up
-    </Link>
-  )
-  return (
     <li className="h6 nav-item">
-      {loginText}
+      <Link to="/login" className="nav-link" style={{ color: "rgb(0,0,0)" }}>
+        Login/Sign up
+      </Link>
     </li>
   );
 }
 
-  class Homey extends React.Component {
-    constructor() {
-      super();
-      this.state = { designPortfolio: []};
-    }
+class Homey extends React.Component {
+  constructor() {
+    super();
+    this.state = { designPortfolio: []};
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
   
-    componentDidMount() {
-      this.loadData();
-    }
-    
 
-    loadData() {
-      setTimeout(() => {
-        this.setState({ designPortfolio: [] }); //to be replaced with database
-      }, 500);
-    }
+  loadData() {
+    setTimeout(() => {
+      this.setState({ designPortfolio: [] }); //to be replaced with database
+    }, 500);
+  }
 
-    render() {
-      const currentLocation = window.location.pathname;
-      const isHome = currentLocation === "/";
-      return (
-        <div className = "container-fluid p-0 sticky-top">
-          <div className="container-fluid item-align-center p-0" style={{position: "sticky", top: "0", zIndex: 100 }}>
-            <div className="row no-gutters align-items-center" style={{backgroundColor: "rgba(255,255,255)"}}>
-              <div className="col-3">
-                <img className="image-rounded p-2"  width="80px" height="80px" id="homeyLogo" src="/homey.png" alt="Homey" />
-              </div>
-              <div className="col-6">
-                <ul className="nav justify-content-center text-center">
-                  <li className="h4 nav-item">
-                    <NavLink to="/home" className="nav-link" style={{color: "rgb(0,0,0)"}}>
-                      Home
-                    </NavLink>
-                  </li>
-                  <li className="h4 nav-item">
-                    <Link to="/designers" className="nav-link" style={{color: "rgb(0,0,0)"}}>
-                      Our Designer
+  render() {
+    const currentLocation = window.location.pathname;
+    const isHome = currentLocation === "/";
+    return (
+      <div className = "container-fluid p-0 sticky-top">
+        <div className="container-fluid item-align-center p-0" style={{position: "sticky", top: "0", zIndex: 100 }}>
+          <div className="row no-gutters align-items-center" style={{backgroundColor: "rgba(255,255,255)"}}>
+            <div className="col-3">
+              <img className="image-rounded p-2"  width="80px" height="80px" id="homeyLogo" src="/homey.png" alt="Homey" />
+            </div>
+            <div className="col-6">
+              <ul className="nav justify-content-center text-center">
+                <li className="h4 nav-item">
+                  <NavLink to="/home" className="nav-link" style={{color: "rgb(0,0,0)"}}>
+                    Home
+                  </NavLink>
+                </li>
+                <li className="h4 nav-item">
+                  <Link to="/designers" className="nav-link" style={{color: "rgb(0,0,0)"}}>
+                    Our Designer
+                  </Link>
+                </li>
+                <li className="h4 nav-item">
+                  <Link to="/gallery" className="nav-link" style={{color: "rgb(0,0,0)"}} >
+                    Gallery
+                  </Link>
+                </li>
+                <li className="h4 nav-item">
+                  <Link to="/contacts" className="nav-link" style={{color: "rgb(0,0,0)"}}>
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className ="col-3">
+              <ul className="nav justify-content-center text-center">
+                  <li className="h6 nav-item">
+                    <Link to="/matchDesigner" className="nav-link" style={{color: "rgb(0,0,0)"}}>
+                      Find My Designer!
                     </Link>
                   </li>
-                  <li className="h4 nav-item">
-                    <Link to="/gallery" className="nav-link" style={{color: "rgb(0,0,0)"}} >
-                      Gallery
-                    </Link>
-                  </li>
-                  <li className="h4 nav-item">
-                    <Link to="/contacts" className="nav-link" style={{color: "rgb(0,0,0)"}}>
-                      Contact
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className ="col-3">
-                <ul className="nav justify-content-center text-center">
-                    <li className="h6 nav-item">
-                      <Link to="/matchDesigner" className="nav-link" style={{color: "rgb(0,0,0)"}}>
-                        Find My Designer!
-                      </Link>
-                    </li>
-                    <LoginStatus/>
-                </ul>
-              </div>
+                  <LoginStatus/>
+              </ul>
             </div>
           </div>
-          {isHome && <Home />}
-         </div>
-      );
-    }
+        </div>
+        {isHome && <Home />}
+        </div>
+    );
   }
+}
+
+function AuthRoute({ component: Component, ...rest }) {
+  const {user} = useContext(AuthContext)
+  return (
+    <Route
+     {...rest}
+     render={(props) =>
+      user ? <Redirect to="/" /> : <Component {...props} />
+    }
+    />
+  )
+}
 
 function App() {
   return (
@@ -117,8 +134,8 @@ function App() {
       <Route path="/contacts" component={Contact}/>
       <Route path="/gallery" component={Gallery}/>
       <Route path="/matchDesigner" component={Match}/>
-      <Route path="/login" component={Login}/>
-      <Route path="/register" component={Register}/>
+      <AuthRoute path="/login" component={Login}/>
+      <AuthRoute path="/register" component={Register}/>
     </Switch>
   </BrowserRouter></AuthProvider>);
 }
